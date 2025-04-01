b914475c4d75f8d8e7855fc5df4003ed47b2d5e4
Jorge5278 <A00837558@tec.mx>
2025-04-01 17:07:34 -0600

    a

--- eslint.config.js
+++ eslint.config.js
@@ -1,39 +1,52 @@
- import js from '@eslint/js'
- import globals from 'globals'
- import react from 'eslint-plugin-react'
- import reactHooks from 'eslint-plugin-react-hooks'
- import reactRefresh from 'eslint-plugin-react-refresh'
+import js from '@eslint/js';
+import globals from 'globals';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import jestPlugin from 'eslint-plugin-jest';  // Importa el plugin de Jest
 
- 
- export default [
-   { ignores: ['dist'] },
-   {
-     files: ['**/*.{js,jsx}'],
-     languageOptions: {
-       ecmaVersion: 2020,
-       globals: globals.browser,
-       parserOptions: {
-         ecmaVersion: 'latest',
-         ecmaFeatures: { jsx: true },
-         sourceType: 'module',
-       },
-     },
-     settings: { react: { version: '18.3' } },
-     plugins: {
-       react,
-       'react-hooks': reactHooks,
-       'react-refresh': reactRefresh,
-     },
-     rules: {
-       ...js.configs.recommended.rules,
-       ...react.configs.recommended.rules,
-       ...react.configs['jsx-runtime'].rules,
-       ...reactHooks.configs.recommended.rules,
-       'react/jsx-no-target-blank': 'off',
-       'react-refresh/only-export-components': [
-         'warn',
-         { allowConstantExport: true },
-       ],
-     },
-   },
- ]
+export default [
+  { ignores: ['dist'] },
+  {
+    files: ['**/*.{js,jsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: {
+        ...globals.browser,
+        describe: 'readable',  // Define como variables globales de Jest
+        it: 'readable',
+        test: 'readable',
+        expect: 'readable',
+        jest: 'readable',
+        beforeEach: 'readable',
+        afterEach: 'readable',
+        beforeAll: 'readable',
+        afterAll: 'readable'
+      },
+      parserOptions: {
+        ecmaVersion: 'latest',
+        ecmaFeatures: { jsx: true },
+        sourceType: 'module',
+      },
+    },
+    settings: { react: { version: '18.3' } },
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      jest: jestPlugin,  // Añade el plugin de Jest
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
+      ...reactHooks.configs.recommended.rules,
+      'react/jsx-no-target-blank': 'off',
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      ...jestPlugin.configs.recommended.rules,  // Añade las reglas recomendadas de Jest
+    },
+  },
+];
